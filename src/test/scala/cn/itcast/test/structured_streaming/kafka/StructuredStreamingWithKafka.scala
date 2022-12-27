a6118c950e3ac269600e505223547ff40e1cbc47
guJiaFu <gjf1016q@yeah.net>
2022-12-27 21:59:21 +0800

    test kafka

---
+++ src/test/scala/cn/itcast/test/structured_streaming/kafka/StructuredStreamingWithKafka.scala
@@ -0,0 +1,75 @@
+package cn.itcast.test.structured_streaming.kafka
+
+import org.apache.commons.lang3.StringUtils
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.streaming.OutputMode
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+
+/**
+ * Author: itcast caoyu
+ * Date: 2021-04-17 0017 15:07
+ * Desc: StructuredStreaming整合Kafka案例
+ * +------------------------------------------------------------+
+ * |value                                                       |
+ * +------------------------------------------------------------+
+ * |station_6,18600002699,18900004912,success,1618645571297,4000|
+ * |station_0,18600008193,18900004960,success,1618645571435,5000|
+ * |station_5,18600007814,18900005173,busy,1618645571155,0      |
+ * +------------------------------------------------------------+
+ */
+object StructuredStreamingWithKafka {
+  def main(args: Array[String]): Unit = {
+
+    // 初始环境
+    val spark: SparkSession = SparkSession.builder()
+      .appName(this.getClass.getSimpleName)
+      .master("local[*]")
+      .config("spark.sql.shuffle.partitions", 2)
+      .getOrCreate()
+    val sc: SparkContext = spark.sparkContext
+    sc.setLogLevel("WARN")
+
+    // 隐式依赖
+    import spark.implicits._
+    import org.apache.spark.sql.functions._
+
+    // source
+    val kafkaDF: DataFrame = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "node1:9092,node2:9092,node3:9092")
+      .option("subscribe", "raw-topic")
+      .option("startingOffsets", "latest")
+//      .option("startingOffsets", "earliest")
+      .load()
+
+    kafkaDF.printSchema()
+
+
+
+    // transform
+    val resultDF: Dataset[String] = kafkaDF
+      .selectExpr("CAST(value as string)")
+      .as[String]
+      .filter(value => StringUtils.isNoneBlank(value) && value.trim.split(",")(3).equals("success"))
+
+    // sink
+
+    //    resultDF.writeStream
+    //      .format("console")
+    //      .outputMode(OutputMode.Append())
+    //      .option("truncate", false)
+    //      .start()
+    //      .awaitTermination()
+
+    resultDF.writeStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "node1:9092")
+      .option("topic", "etl-topic")
+      .option("checkpointLocation", "data/ckp/kafka")
+      .outputMode(OutputMode.Append())
+      .start()
+      .awaitTermination()
+
+    sc.stop
+  }
+}
---
+++ src/test/scala/cn/itcast/test/structured_streaming/kafka/iot/IOTStreamingAnalysisSQL.scala
@@ -0,0 +1,80 @@
+package cn.itcast.test.structured_streaming.kafka.iot
+
+import cn.itcast.spark.structured_streaming.kafka.iot.MockIotDatas.DeviceData
+import com.alibaba.fastjson.JSON
+import org.apache.commons.lang3.StringUtils
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.streaming.OutputMode
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+
+/**
+ * Author: itcast caoyu
+ * Date: 2021-04-17 0017 16:36
+ * Desc: SQL风格从kafka消费数据进行处理IOT数据
+ *
+ * {"device":"device_73","deviceType":"bigdata","signal":77.0,"time":1618648888346}
+ */
+object IOTStreamingAnalysisSQL {
+  def main(args: Array[String]): Unit = {
+
+    /**
+     * 数据处理
+     *  1）、信号强度大于30的设备；
+     *  2）、各种设备类型的数量；
+     *  3）、各种设备类型的平均信号强度；
+     */
+
+    // 初始环境
+    val spark: SparkSession = SparkSession.builder()
+      .appName(this.getClass.getSimpleName)
+      .master("local[*]")
+      .config("spark.sql.shuffle.partitions", 2)
+      .getOrCreate()
+    val sc: SparkContext = spark.sparkContext
+    sc.setLogLevel("WARN")
+
+    // 隐式依赖
+    import spark.implicits._
+    import org.apache.spark.sql.functions._
+
+    // 输入
+    val kafkaDF: DataFrame = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "node1:9092,node2:9092,node3:9092")
+      .option("subscribe", "iotTopic")
+      .option("startingOffsets", "earliest")
+      .load()
+
+    // 转换
+    val deviceDF: Dataset[DeviceData] = kafkaDF.selectExpr("CAST(value as string)")
+      .as[String]
+      .filter(StringUtils.isNoneBlank(_))
+      .map(line => {
+        JSON.parseObject(line, classOf[DeviceData])
+      })
+
+    deviceDF.printSchema()
+
+    deviceDF.createOrReplaceTempView("device_t")
+    val resultDF: DataFrame = spark.sql(
+      """
+        |select
+        |deviceType,
+        |count(*) as cnt,
+        |round(avg(signal), 2) as avg_signal
+        |from device_t
+        |where signal >30
+        |group by deviceType
+        |order by cnt
+        |""".stripMargin)
+
+    // 输出
+    resultDF.writeStream
+      .format("console")
+      .outputMode(OutputMode.Complete())
+      .start()
+      .awaitTermination()
+
+    sc.stop()
+  }
+}
